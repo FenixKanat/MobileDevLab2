5ee8a730e4a3eddbbb79330e98e956bd05837aab
roseygirlforra <fenix.k.97@outlook.com>
2023-09-09 16:29:37 +0200

    Connected to firebase db and now its displaying it's content

--- app/src/main/java/com/example/lab2/MainActivity.kt
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.lab2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab2.ui.theme.Lab2Theme
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
@@ -82,7 +85,15 @@ fun CompaniesScreen() {
 
 @Composable
 fun DisplayCompanies(companies: List<Company>) {
-    Text(text = companies.first().title)
+
+    Column(modifier = Modifier.padding(16.dp)){
+    for(company in companies) {
+        Text(text = "City: ${company.city}")
+        Text("ID: ${company.id}")
+        Text("Title: ${company.title}")
+        Text("Webpage: ${company.webpage}")
+    }
+    }
 
 
 }
